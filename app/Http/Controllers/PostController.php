845133d7c9db9411a5a0d18dec1610598ed89fd6
Moein58010 <m.alizade58010@gmail.com>
2021-07-25 20:01:14 +0430

    13_Migrations

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,39 +2,9 @@
 
 namespace App\Http\Controllers;
 
-// ایمپورت کلاس هایی که معمولا توی کنترلر ها استفاده می شوند
-use App\Models\Post;
-use Database\Seeders\DatabaseSeeder;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
-    // show posts
-    // اسم تابع باید گویای کار و وظیفه ی آن باشد
-    public function show($slug)
-    {
-
-// گرفتن اطلاعات جدول posts از دیتابیس به شرطی که عنوان آن پست برابر با عنوانی که کاربر درخواست داده باشد و اولین رکورد با این ویژگی را باز می گرداند
-        // $post = DB::table('posts')->where('slug', $slug)->first();
-
-//        در وقع همان کار کد بالا را با این تفات که از مدل استفاده می شود، انجام می دهد
-        // $post = Post::query()->where('slug', $slug)->first();
-
-        // firstOrFail() => اگه پست ما خالی بود، صفحه 404 را نشان بده یا بصورت زیر باید بنویسیم
-        // if(empty($post)){
-        //   abort(404);
-        // }
-
-        $post = Post::query()->where('slug', $slug)->firstOrFail();
-
-
-
-        return view('post', [
-            // key => value
-            // 'name' = key or index => حتما باید هم نام با متغیر که توی ویو مشخص کردیم باشد
-            'post' => $post
-        ]);
-
-    }
+    //
 }
---
+++ database/migrations/2021_07_25_152301_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+// ایجاد فیلد آیدی به عنوان کلید اصلی و بدون علامت
+            $table->id();
+
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('body');
+            
+// ایجاد فیلد های تاریخ ایجاد و آپدیت
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
